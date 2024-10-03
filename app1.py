dbfcb3e55abfc25447f9c9d604ab63acc323c09a
VaibhavR1909 <vaibhav1ga20is167@gmail.com>
2024-10-03 23:20:11 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -7,9 +7,7 @@ from flask import Flask
 # current module (__name__) as argument.
 app = Flask(__name__)
 
-# The route() function of the Flask class is a decorator,
-# which tells the application which URL should call
-# the associated function.
+
 @app.route('/')
 # ‘/’ URL is bound with hello_world() function.
 def hello_world():
@@ -20,7 +18,7 @@ if __name__ == '__main__':
 
     # run() method of Flask class runs the application
     # on the local development server.
-    app.run()
+    app.run(host='0.0.0.0', port=5000)
 
 
 
